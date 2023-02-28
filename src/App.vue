f00c179e11e12c34c323f164e7500170c2b89d25
dspopravko <demid1498@gmail.com>
2023-02-28 20:57:29 +0400

    feat: basic authentication and routing

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
+  <div v-if="appReady" class="min-h-full font-Poppins box-border">
+    <Navigation />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navigation from "./components/Navigation.vue";
+import { ref } from "vue";
+import { supabase } from "./supabase/init";
+import store from "./store/index";
 export default {
+  components: {
+    Navigation,
+  },
   setup() {
-    // Create data / vars
-
-    // Check to see if user is already logged in
-
-    // If user does not exist, need to make app ready
-
-    // Runs when there is a auth state change
-    // if user is logged in, this will fire
-
-    return {};
+    const appReady = ref(null);
+    const user = supabase.auth.user();
+    if (!user) {
+      appReady.value = true;
+    }
+    supabase.auth.onAuthStateChange((_, session) => {
+      console.log("Hello!");
+      store.methods.setUser(session);
+      appReady.value = true;
+    });
+    return { appReady };
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Create from "../views/Create.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/create",
+    name: "Create",
+    component: Create,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
 ];
 
